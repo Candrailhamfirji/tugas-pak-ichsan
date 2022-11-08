bb69305424857779002ca9a4d4db8d4b7c4b9e47
Candrailhamfirji <candrafirji@gmail.com>
2022-11-08 17:18:09 +0700

    upload tugas pertama

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+// import 'package:aplikasi_flutter_candra/column_widget.dart';
+import 'package:aplikasi_flutter_candra/row_widget.dart';
+import 'package:aplikasi_flutter_candra/ui/produk_form.dart';
+// import 'package:aplikasi_flutter_candra/hello_world.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+@override
+Widget build(BuildContext context) {
+return const MaterialApp(
+    title: "Aplikasi flutter candra",
+    home: ProdukForm(),
+     );
+}
+}
