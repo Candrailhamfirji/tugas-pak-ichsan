40704ffc07d74cf95226dd17c88409fa47939bab
Candrailhamfirji <candrafirji@gmail.com>
2022-11-15 14:02:05 +0700

    Menambah fitur simpan

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 // import 'package:aplikasi_flutter_candra/column_widget.dart';
-import 'package:aplikasi_flutter_candra/row_widget.dart';
+// import 'package:aplikasi_flutter_candra/row_widget.dart';
 import 'package:aplikasi_flutter_candra/ui/produk_form.dart';
 // import 'package:aplikasi_flutter_candra/hello_world.dart';
 import 'package:flutter/material.dart';
